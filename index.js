711bfe6c6ea393a5b739ec552f0b01619735a4b4
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-12-25 18:31:47 -0500

    building the menu item logic

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log(trafficCameras);
+
+function buildMenuItem(camera){
+    let menuItem = document.createElement('div');
+
+    menuItem.id = camera.number;
+    
+}
+
+function buildMenu(){
+    let nav = document.querySelector('nav');
+
+    trafficCameras.forEach((camera) => {
+        let cameraMenuItem = buildMenuItem(camera);
+        nav.appendChild(cameraMenuItem);
+    });
+}
+
+
+window.onload = function (){
+    buildMenu();
+}
