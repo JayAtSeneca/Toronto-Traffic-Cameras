901a88499625ddfa678337e9acbec8e000ecdced
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-12-26 00:34:27 -0500

    updated the index js with the image update logic

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ function buildMenuItem(camera){
     let menuItem = document.createElement('div');
 
     menuItem.id = camera.number;
-    
+    menuItem.className = 'menu-item';
+    menuItem.title = camera.name;
+    menuItem.innerText = camera.name;
+    return menuItem;
 }
 
 function buildMenu(){
@@ -13,9 +16,51 @@ function buildMenu(){
         let cameraMenuItem = buildMenuItem(camera);
         nav.appendChild(cameraMenuItem);
     });
+
+    nav.addEventListener('click', (event)=>{
+        let cameraNumber = event.target.id;
+        let camera = trafficCameras.find((camera)=>{
+            return camera.number === cameraNumber;
+        });
+        updateMap(camera.lat, camera.lng);
+        updateImages(camera);
+    })
 }
 
+function updateImages(camera){
+    let imagesDiv = document.querySelector('#images');
+    let cameraImage = document.querySelector('#camera-image');
+
+    cameraImage.src = camera.getImageUrl();
+
+    imagesDiv.classList.remove('hidden');
+
+}
+
+let map;
+let marker
+
+function updateMap(lat, lng) {
+  // Move or Create a marker to this intersection
+  if(marker) {
+    marker.setLatLng({ lat, lng });
+  } else {
+    marker = L.marker([lat, lng]).addTo(map);
+  }
+
+  // Update the map's location
+  map.setView([lat, lng]);
+}
+
+function buildMap(){
+    let senecaCoords = [43.7952, -79.3497];
+    map = L.map('map').setView(senecaCoords, 16);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
 
 window.onload = function (){
     buildMenu();
+    buildMap();
 }
